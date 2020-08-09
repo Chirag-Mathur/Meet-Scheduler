1c6ef9b30b4b946f5eadab0eb1a359be3382545e
Chirag-Mathur <chiragmathurindia@gmail.com>
2020-08-09 23:50:56 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,14 +6,14 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/NOTICES": "8b7ed4c18ab529a4180e174aa1eaaf83",
+"assets/NOTICES": "154ed7a575b5c106287607487f58fa67",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "580af7b5d959b9b1381be6374974a6d3",
 "/": "580af7b5d959b9b1381be6374974a6d3",
-"main.dart.js": "95ac9ecdcd491ac4fa6d2f6a21d08fd2",
+"main.dart.js": "1d98ab6131aa2b037daa352c33bf58fc",
 "manifest.json": "799078203efafab93b9984514bb7024c"
 };
 
