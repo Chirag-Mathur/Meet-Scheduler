f795cbea1ae06ea153f8ccf0805a99d1e2d98da7
Chirag-Mathur <chiragmathurindia@gmail.com>
2020-08-13 23:24:25 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,17 +3,18 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+  "assets/AssetManifest.json": "1b619447022f0db134b88f56fcceb161",
+"assets/Assets/Images/timetable.jpg": "49c81778ce52760a1cc8e41da08782ad",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/NOTICES": "154ed7a575b5c106287607487f58fa67",
+"assets/NOTICES": "980fa6568d784a58c2640777d69cf27a",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "580af7b5d959b9b1381be6374974a6d3",
 "/": "580af7b5d959b9b1381be6374974a6d3",
-"main.dart.js": "8fe5aae5cd5c20b337da3f24310099b2",
+"main.dart.js": "0728985134967e828549150907e92bac",
 "manifest.json": "799078203efafab93b9984514bb7024c"
 };
 
